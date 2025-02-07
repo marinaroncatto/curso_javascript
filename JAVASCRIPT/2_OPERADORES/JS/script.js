18679b2c7b0338433af4c55c95dcaa3dbda87c53
marinaroncatto <marinam.roncatto@gmail.com>
2025-02-07 20:19:09 -0300

    Estudos 07/02

---
+++ JAVASCRIPT/2_OPERADORES/JS/script.js
@@ -0,0 +1,22 @@
+// 1 - Number
+
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -128);
+console.log(typeof 2);
+
+// 2 - Operações aritméticas
+
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(10 / 5);
+console.log(2 * 4);
+console.log(2 +  4 * 2);
+
+// 3 - Special Numbers
+
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof infinity); // case sensitive
+console.log(121 * "asd");
+console.log(typeof NaN);
