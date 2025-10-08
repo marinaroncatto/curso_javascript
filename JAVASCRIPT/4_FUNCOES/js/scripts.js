3a8ecb79563e0256173b562bd2d5d2b7f82d02b8
marinaroncatto <marinam.roncatto@gmail.com>
2025-10-08 18:04:29 -0300

    Finalizando unidade 4

--- JAVASCRIPT/4_FUNCOES/js/scripts.js
+++ JAVASCRIPT/4_FUNCOES/js/scripts.js
@@ -125,3 +125,114 @@ const helloWorld = () => console.log("Hello World");
 helloWorld();
 
 console.log(helloWorld()); //undefined pois o console.log já foi exibido e o segundo fica sem argumento
+
+
+// 7 - parametro opcional
+
+const multiplication = function(m, n){
+    if(n === undefined){
+        return m * 2;
+    }
+    else {
+        return m * n;
+    }
+}
+
+console.log(multiplication(5));
+
+console.log(multiplication(2, 4));
+
+const greeting = (name) => {
+    if(!name){
+        console.log("Olá!");
+        return
+    }
+    console.log(`Olá ${name}!`);
+}
+
+greeting();
+greeting("Matheus");
+
+// 8 - valor default 
+
+const customGreeting = (name, greet = "Olá") => {
+    return `${greet}, ${name}`;
+};
+
+console.log(customGreeting("Matheus"));
+
+console.log(customGreeting("João", "Bom dia"));
+
+const repeatText = (text, repeat = 2) => {
+    for(let i = 0; i < repeat; i ++){
+        console.log(text);
+    }
+};
+
+repeatText("Testando");
+
+repeatText("Agora repete 5 vezes", 5);
+
+// 9 - closure
+
+function someFunction(){
+    let txt = "Alguma coisa";
+
+    function display(){
+        console.log(txt);
+    }
+    display();
+}
+
+someFunction();
+
+//10 - mais sobre closure
+
+const multiplicantionCLosure = (n) => {
+    return (m) => {
+        return n * m;
+    };
+};
+
+const c1 = multiplicantionCLosure(5);
+
+const c2 = multiplicantionCLosure(10);
+
+console.log(c1);
+
+console.log(c2);
+
+console.log(c1(5));
+
+console.log(c2(10));
+
+//11 - recursion
+
+const untilTen = (n, m) => {
+    if( n < 10){
+        console.log("A função parou de executar!");
+    }else{
+        const x = n - m;
+
+        console.log(x);
+
+        untilTen(x, m);
+    }
+};
+
+untilTen(100, 7);
+
+
+function factorial(x) {
+    if( x === 0){
+        return 1;
+    }else{
+        return x * factorial(x -1);
+    }
+}
+
+const num = 6;
+
+const result = factorial(num);
+
+console.log(`O fatorial do número ${num} é ${result}`);
